e0306f004cb03423fdad3d373569874f2439ceef
OrkunAvci <orkunavci28@gmail.com>
2022-01-08 16:28:14 +0300

    Clean up

--- main.py
+++ main.py
@@ -14,31 +14,3 @@ for i in range(4):
 	p_img = pi.process(img)
 	io.imshow(p_img)
 	io.show()
-
-
-#  Dump
-"""
-#   Get image
-image = cv.imread("./resources/dog.pgm", 0)
-#image = ski_color.rgb2gray(data.rocket())
-edges = filters.sobel(image)
-
-#   Normalize
-prev_max = edges.max()
-limit = edges.mean()
-top_arr = []
-for row in edges:
-	sub_arr = []
-	for cell in row:
-		if (cell / prev_max) > 0.01:
-			sub_arr.append(cell / prev_max)
-		else:
-			sub_arr.append(0.0)
-	top_arr.append(sub_arr)
-edges = numpy.array(top_arr)
-
-#   Output
-print(edges.min(), edges.max(), edges.mean())
-io.imshow(edges)
-io.show()
-"""
