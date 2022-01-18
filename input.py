2231c5d81474925687af9e9bb1e5eb84222e91a7
OrkunAvci <orkunavci28@gmail.com>
2022-01-18 08:39:39 +0300

    More robust

--- input.py
+++ input.py
@@ -1,12 +1,13 @@
 # Dummy file
 import cv2 as cv
+import os
 
 dummy = {
 	"path": "./resources/Scenario_1/",
 	"file_no": 0,
-	"file_extension": ".jpg",
-	"max_frame": 6
+	"file_extension": ".jpg"
 }
+dummy["max_frame"] = len([name for name in os.listdir(dummy["path"]) if os.path.isfile(os.path.join(dummy["path"], name)) if name.endswith(dummy["file_extension"])])
 
 def get_next():
 	file = dummy["path"] + str(dummy["file_no"]) + dummy["file_extension"]
